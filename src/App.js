8a024ab07af50051348cdd73b4f6f5bdf7acee76
vishnu3254 <124258973+vishnu3254@users.noreply.github.com>
2023-06-08 12:19:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,32 +12,15 @@ function App() {
   // state to store all tasks
   const [tasks, setTasks] = useState([]);
 
+  //state to know if the task is added   
   const [taskAdded, setTaskAdded] = useState(false);
 
-  const [counter, setCounter] = useState(0);
 
   // getting the props from child for whenever task is added getting the data from localstorage
   const handleDataFromChild = (data) => {
     // console.log("Data received from child:", data);
     setTaskAdded(true);
   };
-
-  // // setting up the email configuration
-  // const transporter = nodemailer.createTransport({
-  //   service: "Gmail",
-  //   auth: {
-  //     user: "vishnuvardhanudagundla7@gmail.com",
-  //     pass: "qkglmtarrrtdlepl",
-  //   },
-  // });
-
-  // // setting up mailOptions
-  // let mailOptions = {
-  //   from: "vishnuvardhanudagundla7@gmail.com",
-  //   to: "vv50285@gmail.com",
-  //   subject: "Task Remainder!!!",
-  //   text: "",
-  // };
 
   // function for scheduling the tasks
   const scheduleTasks = (allTasks) => {
@@ -55,15 +38,12 @@ function App() {
         const job = schedule.scheduleJob(targetDate, async () => {
           // console.log("Hey hi this is reminding you...", task.name);
           const taskDescription = task.name;
-          // console.log("hello");
-          // Perform the tasks for the specific date
           let res = await axios.post("http://localhost:4000/send-mail", {
             name: taskDescription,
           });
           console.log("response", res);
         });
-        console.log("all tasks after scheduling", allTasks);
-        console.log("counter is", counter);
+       
       }
       task.schedule = true;
       //update locallstorage
